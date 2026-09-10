900a0dcac33f3dc3eea372b0c7c015c6d057274e
Lanshiboi <168156338+Lanshiboi@users.noreply.github.com>
2026-09-10 20:20:14 +0800

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-resume/',
+  base: '/lansh-3d-portfolio/',
   plugins: [react()],
 })
